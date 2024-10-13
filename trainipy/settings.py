e147aa779908153c53511b1341bf4f6384a18326
Andrii <andrii.khamaza0@gmail.com>
2024-10-13 10:14:10 +0300

    add user model

--- trainipy/settings.py
+++ trainipy/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "station",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -123,4 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL =
+AUTH_USER_MODEL = "user.User"
